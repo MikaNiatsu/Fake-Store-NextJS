b894329d0105dc40f8d6e473ee3feb932deb2e55
Miguel Linares <107733760+MikaNiatsu@users.noreply.github.com>
2024-09-21 17:09:51 -0500

    First Dev

---
+++ fake-store/src/app/login/forgotPassword/page.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+
+export default function ForgotPassword() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-lg shadow-md border border-pink-200">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-pink-600">
+            Reset your password
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Enter your email address, and we&#39;ll send you a link to reset
+            your password.
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+            >
+              Send Reset Link
+            </button>
+            
+          </div>
+          <div className="flex items-center justify-between">
+              <Link
+                href="/login"
+                className="font-medium text-pink-600 hover:text-pink-500"
+              >
+                Back to Login
+              </Link>
+            </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ fake-store/src/app/login/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+//import Image from "next/image";
+export default function Login() {
+  return (
+    <>
+        <div className="min-h-screen flex items-center justify-center bg-white">
+        <motion.div
+      whileHover={{ scale: 1.1 }}
+      transition={{ type: "spring", stiffness: 400, damping: 25 }}>
+        <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-lg shadow-md border border-pink-200">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-pink-600">
+              Sign in to your account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" value="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address" />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                  placeholder="Password" />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="text-sm">
+                <Link
+                  href="/login/forgotPassword"
+                  className="font-medium text-pink-600 hover:text-pink-500"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+              >
+                Sign in
+              </button>
+
+              <Link
+                href="/newUser"
+                type="button"
+                className="mt-2 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+              >
+                Create Account
+              </Link>
+            </div>
+          </form>
+        </div>
+        </motion.div>
+      </div>
+    </>
+    
+  );
+}
+
